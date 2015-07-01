ad9d0e2e3a46089e60f37805136fec5f9b287394
Jimmy Abner <jimmy.abner@artelys.com>
2015-07-01 14:33:13 +0200

    add pom to build.

--- src/com/bombo/pursuit/Main.java
+++ src/com/bombo/pursuit/Main.java
@@ -25,12 +25,21 @@ public class Main extends Application implements Observer {
  
     @Override
     public void start(Stage primaryStage) {
+    	Parameters parameters = getParameters();
+    	List<String> args = parameters.getRaw();
+    	int size = 5; 
+    	if(args.size()>0) {
+    		try {
+    			size = Integer.parseInt(args.get(0));
+    		} catch (NumberFormatException e) {
+    			System.err.println("The first argument must be an integer.");
+    		}
+    	}
+    	
         primaryStage.setTitle("Drawing Operations Test");
         Group root = new Group();
         int canvaWidth = 700;
         Canvas canvas = new Canvas(canvaWidth, canvaWidth);
-        
-        int size = 10;
         
         gc = canvas.getGraphicsContext2D();
         double[][] points = strokePolygon(gc, 350, 350, size, 300);
@@ -39,7 +48,7 @@ public class Main extends Application implements Observer {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         
-        PuirsuitThread pt = new PuirsuitThread(points, size, 10);
+        PuirsuitThread pt = new PuirsuitThread(points, size, 15);
         pt.addObserver(this);
         new Thread(pt).start();
         
@@ -61,8 +70,6 @@ public class Main extends Application implements Observer {
     		xPoints[i] = width * Math.cos(2*Math.PI*i/point) + x;
     		yPoints[i] = width * Math.sin(2*Math.PI*i/point) + y;
     	}
-   		//gc.fillPolygon(xPoints, yPoints, point);
-	    //gc.fillText("");
 	   	gc.strokePolygon(xPoints, yPoints, point);
 	   	return new double[][]{xPoints, yPoints};
     }
@@ -77,7 +84,7 @@ public class Main extends Application implements Observer {
 					for(Object p : (List<?>)arg) {
 						if(p instanceof Point) {
 							gc.setFill(Color.GREEN);
-					    	gc.fillOval(((Point) p).getX(), ((Point) p).getY(), 5, 5);
+					    	gc.fillOval(((Point) p).getX()-2.5, ((Point) p).getY()-2.5, 5, 5);
 							if(lastPoint != null) {
 								gc.strokeLine(lastPoint.getX(), lastPoint.getY(), ((Point) p).getX(), ((Point) p).getY());
 							}
@@ -85,12 +92,7 @@ public class Main extends Application implements Observer {
 						}
 					}
 					gc.strokeLine(lastPoint.getX(), lastPoint.getY(), ((Point)((List<?>) arg).get(0)).getX(), ((Point)((List<?>) arg).get(0)).getY());
-					gc.setFill(Color.WHITE);
-					gc.fillRect(0, 0, 700, 16);
-					gc.setFill(Color.GREEN);
-					gc.fillText(((Point)((List<?>) arg).get(0)).getX() + ", " + ((Point)((List<?>) arg).get(0)).getY(), 300, 12);
 				}
-				System.out.println("updated!");
 			}
 		});	
 	}
--- test/com/bombo/pursuit/PointTest.java
+++ test/com/bombo/pursuit/PointTest.java
@@ -9,23 +9,23 @@ public class PointTest {
 	@Test
 	public void testMove() {
 		Point p = new Point(0, 0, 1, null);
-		Point p2 = new Point(0, 1, 1, p);
+		Point p2 = new Point(0, 100, 1, p);
 		System.out.println(p2.move());
 		assertEquals(0, p2.getX(), 0);
-		assertEquals(0, p2.getY(), 0);
+		assertEquals(99, p2.getY(), 0);
 		
 
 		p = new Point(0, 0, 1, null);
-		p2 = new Point(0, 2, 1, p);
+		p2 = new Point(0, 100, 1, p);
 		System.out.println(p2.move());
 		assertEquals(0, p2.getX(), 0);
-		assertEquals(0, p2.getY(), 1);
+		assertEquals(99, p2.getY(), 0);
 		
 
 		p = new Point(0, 0, 1, null);
-		p2 = new Point(1, 0, 1, p);
+		p2 = new Point(100, 0, 1, p);
 		System.out.println(p2.move());
-		assertEquals(0, p2.getX(), 0);
+		assertEquals(99, p2.getX(), 0);
 		assertEquals(0, p2.getY(), 0);
 	}
 
